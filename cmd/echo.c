2047c36bf13ce213d8d7dc0783524b5250716321
CTCaer <ctcaer@gmail.com>
2023-02-21 09:31:14 -0500

    cmd: echo: add output to stderr support
    -e flag redirects output to stderr
    Based on https://gitlab.com/switchroot/bootstack/switch-uboot/-/commit/70d82f9500cecb63affd1fd5ed73db7d5265a6cf
    
    Co-authored-by: CTCaer <ctcaer@gmail.com>
    Co-authored-by: Thomas Makin <halorocker89@gmail.com>

--- cmd/echo.c
+++ cmd/echo.c
@@ -7,30 +7,62 @@
 #include <common.h>
 #include <command.h>
 
+static void echo_char(const char c, bool err)
+{
+	if (!err)
+		putc(c);
+	else
+		eputc(c);
+}
+
+static void echo_string(const char *s, bool err)
+{
+	if (!err)
+		puts(s);
+	else
+		eputs(s);
+}
+
 static int do_echo(struct cmd_tbl *cmdtp, int flag, int argc,
 		   char *const argv[])
 {
 	int i = 1;
+    bool err = false;
 	bool space = false;
 	bool newline = true;
 
-	if (argc > 1) {
-		if (!strcmp(argv[1], "-n")) {
-			newline = false;
-			++i;
-		}
-	}
+    if (argc > 1) {
+        if (!strcmp(argv[1], "-n")) {
+            newline = false;
+            ++i;
+        }
+        if (!strcmp(argv[1], "-e")) {
+            err = true;
+            ++i;
+        }
+    }
+
+    if (argc > 2 && (!newline || err)) {
+        if (!strcmp(argv[2], "-n")) {
+            newline = false;
+            ++i;
+        }
+        if (!strcmp(argv[2], "-e")) {
+            err = true;
+            ++i;
+        }
+    }
 
 	for (; i < argc; ++i) {
 		if (space) {
-			putc(' ');
+			echo_char(' ', err);
 		}
-		puts(argv[i]);
+		echo_string(argv[i], err);
 		space = true;
 	}
 
 	if (newline)
-		putc('\n');
+		echo_char('\n', err);
 
 	return 0;
 }
@@ -38,6 +70,6 @@ static int do_echo(struct cmd_tbl *cmdtp, int flag, int argc,
 U_BOOT_CMD(
 	echo, CONFIG_SYS_MAXARGS, 1, do_echo,
 	"echo args to console",
-	"[-n] [args..]\n"
-	"    - echo args to console; -n suppresses newline"
+	"[-e] [-n] [args..]\n"
+	"    - echo args to console; -e prints to stderr; -n suppresses newline;"
 );
