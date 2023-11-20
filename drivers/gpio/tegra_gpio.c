CTCaer/switch-l4t-uboot

arch: tegra: add gpio to sfio config
Allow setting a pin to SFIO from gpio config table with `TEGRA_GPIO_INIT_SFIO`.

drivers/gpio/tegra_gpio.c
@@ -217,6 +217,9 @@ void gpio_config_table(const struct tegra_gpio_config *config, int len)
 			set_level(config[i].gpio, 1);
 			set_direction(config[i].gpio, DIRECTION_OUTPUT);
 			break;
+		case TEGRA_GPIO_INIT_SFIO:
+			set_config(config[i].gpio, CONFIG_SFIO);
+			continue;
 		}
 		set_config(config[i].gpio, CONFIG_GPIO);
 	}

LineageOS/android_hardware_nintendo_u-boot

drivers/gpio/tegra_gpio.c
@@ -215,6 +215,9 @@ void gpio_config_table(const struct tegra_gpio_config *config, int len)
 			set_level(config[i].gpio, 1);
 			set_direction(config[i].gpio, DIRECTION_OUTPUT);
 			break;
+		case TEGRA_GPIO_INIT_SFIO:
+			set_config(config[i].gpio, CFG_SFIO);
+			continue;
 		}
 		set_config(config[i].gpio, CFG_GPIO);
 	}